0489a6b0c7bfe34b1e472814045b3dbee5dab797
rafe520 <rafalmichalik@op.plp>
2018-07-26 14:10:13 +0200

    Dołożenie WWW
    Konfiguracja plików urls i views i dołozenie strony html

--- news/views.py
+++ news/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
 
-# Create your views here.
+def index(request):
+    return render(request, 'news/index.html', {'zmienna': 'Jestem widokiem'})
+
+
+
+
